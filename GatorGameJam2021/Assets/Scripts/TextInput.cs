48adec34587c9428e32c1d68fdacb40f887eb671
acomiskey <acomiskey@ufl.edu>
2021-07-24 20:56:30 -0400

    Added some parsing.

--- GatorGameJam2021/Assets/Scripts/TextInput.cs
+++ GatorGameJam2021/Assets/Scripts/TextInput.cs
@@ -14,12 +14,57 @@ public class TextInput : MonoBehaviour
 
     public void onInput()
     {
+        string [] args = cmdinput.text.Split(' ');
+        cmdinput.text = "> " + cmdinput.text;
+        pushUp();
+
+        if(args.Length > 2)
+        {
+            cmdinput.text = "that's too complicated. i should use less words.";
+        }
+        else if(args[0].Equals("use"))
+        {
+            if(currenttile.use.ContainsKey(args[1]))
+            {
+                //change current tile
+                cmdinput.text = currenttile.use[args[1]];
+            }
+            else cmdinput.text = "I can't use a "+ args[1]+ ".";
+        }
+        else if(args[0].Equals("take"))
+        {
+            if(currenttile.take.ContainsKey(args[1]));
+        }
+        else if(args[0].Equals("look"))
+        {
+
+        }
+        else if(args[0].Equals("move"))
+        {
+            
+        }
+        else if(args[0].Equals("hide"))
+        {
+            if(currenttile.hide != null)
+            {
+                //go hide
+            }
+        }
+        else
+        {
+            cmdinput.text = "i don't know how to "+ args[0]+".";
+        }
+        pushUp();
+        return;
+    }
+
+    public void pushUp()
+    {
         latest5.text = latest4.text;
         latest4.text = latest3.text;
         latest3.text = latest2.text;
         latest2.text = latest1.text;
         latest1.text = cmdinput.text;
         cmdinput.text = "";
-        return;
     }
 }
